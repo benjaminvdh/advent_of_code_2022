cf433a31ea52ab2200fbefe3af739a96293d8654
benjaminvdh <4235118+benjaminvdh@users.noreply.github.com>
2022-12-02 20:11:11 +0100

    Move all of Day 1 into a single file

--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -1,7 +1,4 @@
-mod parsing;
-mod part_1;
-mod part_2;
-
+use crate::parsing::*;
 use crate::solving::*;
 
 type Input = Vec<u64>;
@@ -12,15 +9,68 @@ impl crate::Solver for Solver {
     type Input = Input;
     const DAY: u8 = 1;
 
-    fn parse<R: Read>(input: BufReader<R>) -> Result<Self::Input, ParseError> {
-        parsing::parse(input)
+    fn parse<R: Read>(mut input: BufReader<R>) -> Result<Self::Input, ParseError> {
+        let mut contents = String::new();
+        let _ = input.read_to_string(&mut contents)?;
+
+        let elves = contents
+            .split("\n\n")
+            .map(|elf| elf.lines().map(|line| line.parse::<u64>()).sum())
+            .collect::<Result<_, _>>()?;
+
+        Ok(elves)
     }
 
     fn part_1(input: Self::Input) -> SolveResult {
-        part_1::solve(input)
+        input.into_iter().max().ok_or(SolveError::EmptyInput)
     }
 
-    fn part_2(input: Self::Input) -> SolveResult {
-        part_2::solve(input)
+    fn part_2(mut input: Self::Input) -> SolveResult {
+        input.sort_unstable();
+        Ok(input.iter().rev().take(3).sum())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::BufReader;
+
+    use crate::solving::Solver;
+
+    use super::Solver as Day1Solver;
+
+    fn get_input() -> Vec<u64> {
+        vec![6000, 4000, 11000, 24000, 10000]
+    }
+
+    #[test]
+    fn parsing() {
+        let input = r"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+        let parsed_input = Day1Solver::parse(BufReader::new(input.as_bytes())).unwrap();
+        assert_eq!(parsed_input, get_input());
+    }
+
+    #[test]
+    fn part_1() {
+        assert_eq!(Day1Solver::part_1(get_input()).unwrap(), 24000);
+    }
+
+    #[test]
+    fn part_2() {
+        assert_eq!(Day1Solver::part_2(get_input()).unwrap(), 45000);
     }
 }

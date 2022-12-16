3c99732b4d3c7da26ae8fb185b76dc5d7a562e0e
benjaminvdh <4235118+benjaminvdh@users.noreply.github.com>
2022-12-16 14:08:34 +0100

    Allow attributes in define_integration_test! macro

--- src/macros.rs
+++ src/macros.rs
@@ -9,7 +9,7 @@ macro_rules! define_main {
 
 #[macro_export]
 macro_rules! define_integration_test {
-    ($day:ident, $number:literal, $answer_1:expr$(, $answer_2:expr)?) => {
+    ($day:ident, $number:literal, $answer_1:expr $( => $attr_1:meta)* $(, $answer_2:expr $( => $attr_2:meta)*)?) => {
         use advent_of_code::$day::Solver;
 
         fn get_input() -> String {
@@ -17,6 +17,7 @@ macro_rules! define_integration_test {
         }
 
         #[test]
+        $(#[$attr_1])*
         fn part_1() {
             let answer = advent_of_code::solve_part_1::<Solver>(get_input()).unwrap();
             assert_eq!(answer, $answer_1);
@@ -24,6 +25,7 @@ macro_rules! define_integration_test {
         $(
 
         #[test]
+        $(#[$attr_2])*
         fn part_2() {
             let answer = advent_of_code::solve_part_2::<Solver>(get_input()).unwrap();
             assert_eq!(answer, $answer_2);
